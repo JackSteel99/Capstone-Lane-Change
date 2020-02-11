9b82577893dc0586e68f16f387ff7074be6ee3ab
Ben Marini <benmarini@xoz.ca>
2020-02-10 20:58:09 -0500

    add sudo code for Line Follow

--- Algorithm/lineFollow.py
+++ Algorithm/lineFollow.py
@@ -1,1 +1,15 @@
+import sys
+sys.path.insert(0, '../Hardware/')
+
+from line_tracker import isInLane
+i
+
+while True:
+    inLane, turnDir = isInLane()
+    if(inLane == r):
+        pass # turn right
+    elif(inLane == l):
+        pass # turn left
+    else:
+        continue
 
