80f2083c8ed44f102be846b0e2efa86a76f40c1c
Ben Marini <benmarini@xoz.ca>
2020-02-11 20:39:43 -0500

    added psedocode for lineFollow and cases.py

--- algorithm/cases.py
+++ algorithm/cases.py
@@ -1,2 +1,9 @@
-def main(){
-        print("will work on this later")
+import threading
+
+def case_checker():
+    pass # which case is this
+
+caseCheck = threading.Thread(target=case_checker, args=(ls, rs, fs)) # args for left, right, front sensors
+caseCheck.daemon = True
+caseCheck.start()
+
--- algorithm/lineFollow.py
+++ algorithm/lineFollow.py
@@ -1,13 +1,19 @@
 import sys
-sys.path.insert(0, '../Hardware/')
+sys.path.insert(0, '../hardware/')
 
 from line_tracker import isInLane
 
 while True:
+    """isInLane(): returns two variables depending on IR sensor readings
+    inLane: boolean, True if both sensors are high / False if either are low
+    turnDir: l or r chars for which direction the car should turn
+
+    """
     inLane, turnDir = isInLane()
-    if(inLane == r):
+
+    if(turnDir == r):
         pass # turn right
-    elif(inLane == l):
+    elif(turnDir == l):
         pass # turn left
     else:
         continue
